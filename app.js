dbf588feaa34f720f284fe709b51128f2e5f6db4
963097377@qq.com <963097377@qq.com>
2019-12-08 15:42:17 +0800

    update

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = new Koa()
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
-// const bodyparser = require('koa-bodyparser')
-const koaBody = require('koa-body');
+const bodyparser = require('koa-bodyparser')
+// const koaBody = require('koa-body');
 
 const logger = require('koa-logger')
 
@@ -15,22 +15,26 @@ const users = require('./routes/user')
 onerror(app)
 
 // middlewares
-// app.use(bodyparser({
-//   enableTypes:['json', 'form', 'text']
-// }))
-app.use(koaBody({
-  multipart:true, // 支持文件上传
-  encoding:'gzip',
-  formidable:{
-    uploadDir:'./public/upload/', // 设置文件上传目录
-    keepExtensions: true,    // 保持文件的后缀
-    maxFieldsSize:2 * 1024 * 1024, // 文件上传大小
-    onFileBegin:(name,file) => { // 文件上传前的设置
-      // console.log(`name: ${name}`);
-      // console.log(file);
-    },
-  }
-}));
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+
+// app.use(async ctx => {
+//   ctx.body = ctx.request.body;
+// })
+// app.use(koaBody({
+//   multipart:true, // 支持文件上传
+//   encoding:'gzip',
+//   formidable:{
+//     uploadDir:'./public/upload/', // 设置文件上传目录
+//     keepExtensions: true,    // 保持文件的后缀
+//     maxFieldsSize:2 * 1024 * 1024, // 文件上传大小
+//     onFileBegin:(name,file) => { // 文件上传前的设置
+//       // console.log(`name: ${name}`);
+//       // console.log(file);
+//     },
+//   }
+// }));
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
@@ -45,6 +49,7 @@ app.use(async (ctx, next) => {
   ctx.set('Access-Control-Allow-Headers', '*');
   ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   ctx.set('Cache-Control', 'no-cache');
+  ctx.set('Content-Type', 'application/json');
   await next();
 })
 // logger
--- controllers/user.js
+++ controllers/user.js
@@ -18,32 +18,37 @@ class userController {
     static async create(ctx){
         //接收客户端
         let req = ctx.request.body;
-        if(req.title && req.author && req.content && req.category){
+        if(req.email && req.password && req.category && req.company_name
+            && req.real_name  && req.phone && req.has_media_contact){
             try{
                 //创建用户模型
-                const ret = await UserModel.createUser(req);
-                //使用刚刚创建的用户ID查询文章详情，且返回用户详情信息
-                const data = await UserModel.getUserDetail(ret.id);
-                ctx.response.status = 200;
-                ctx.body = {
-                    code: 200,
-                    msg: '创建用户成功',
-                    data
+                const hasEmail = await User.findOne({
+                    where: {
+                        email: req.email
+                    }
+                });
+                if(hasEmail) {
+                    OK(ctx, 300, '邮箱已经存在！', null);
+                }
+                else {
+                    const ret = await User.create(req);
+                    //使用刚刚创建的用户ID查询文章详情，且返回用户详情信息
+                    const data = await User.findOne({
+                        where: {
+                           id: ret.id
+                        }
+                    });
+                   if(data) {
+                    OK(ctx, 200, '用户创建成功！', data);
+                   } else {
+                    OK(ctx, 300, '用户创建失败，请稍后重试！', null);
+                   }
                 }
             }catch(err){
-                ctx.response.status = 412;
-                ctx.body = {
-                    code: 412,
-                    msg: '创建用户失败',
-                    data: err
-                }
+                OK(ctx, 300, '用户创建失败，请稍后重试', hasEmail);
             }
         }else {
-            ctx.response.status = 416;
-            ctx.body = {
-                code: 200,
-                msg: '参数不齐全'
-            }
+            OK(ctx, 300, '参数不齐全！', req);
         }
     }
 
@@ -57,27 +62,23 @@ class userController {
         if(id){
             try{
                 // 查询用户详情模型
-                let data = await UserModel.getUserDetail(id);
-                ctx.response.status = 200;
-                ctx.body = {
-                    code: 200,
-                    msg: '查询成功',
-                    data
+                let data = await UserModel.findOne({
+                    where: {
+                       id: id
+                    }
+                });
+                if(data) {
+                    OK(ctx, 200, '查询成功', data);
                 }
+                else {
+                    OK(ctx, 300, '为查询到数据', data);
+                }
+               
             }catch(err){
-                ctx.response.status = 412;
-                ctx.body = {
-                    code: 412,
-                    msg: '查询失败',
-                    data
-                }
+                OK(ctx, 300, '查询失败', err);
             }
         }else {
-            ctx.response.status = 416;
-            ctx.body = {
-                code: 416,
-                msg: '用户ID必须传'
-            }
+            OK(ctx, 300, '用户ID必须传', null);
         }
     }
     /**
@@ -86,7 +87,7 @@ class userController {
      * @returns {Promise.<void>}
      */
     static async login(ctx){
-        let req = ctx.query;
+        let req = ctx.request.body;
         if(req.email && req.password){
             try{
                 //创建用户模型
@@ -99,21 +100,17 @@ class userController {
                    const password = ret.password;
                    if(password === req.password) {
                        OK(ctx, 200 ,'登录成功', null);
+                   } else {
+                    OK(ctx, 300 ,'密码错误', null);
                    }
                } else {
-                OK(ctx, "app_404" ,'您还未注册，请先注册', null)
+                OK(ctx, 300 ,'您还未注册，请先注册', null)
                }
             }catch(err){
-                OK(ctx, "app_407" ,'登录失败', err)
+                OK(ctx, 300 ,'登录失败', err)
             }
         }else {
-            ctx.response.status = 416;
-            ctx.body = {
-                code: "app_407",
-                msg: '参数不齐全',
-                data: ctx.request,
-                data1: ctx.params,
-            }
+            OK(ctx, 300 ,'参数不齐全', null)
         }
     }
 }
