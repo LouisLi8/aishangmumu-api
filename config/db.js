7440107c5595ee9f974cb4465bb6fe10a2f803bc
lining@yougu.tech <lining@yougu.tech>
2020-04-17 17:29:39 +0800

    update

--- config/db.js
+++ config/db.js
@@ -1,11 +1,7 @@
 const Sequelize = require('sequelize');
 // const sequelize = new Sequelize('aishangmumu','root','root',{
 const sequelize = new Sequelize('asmm','asmm','asmmadmin',{
-<<<<<<< HEAD
-    host:'127.0.0.1',
-=======
     host:'39.100.196.120',
->>>>>>> d5da41dd08a277737e2ef6d3a6ab44b039bffb6a
     dialect:'mysql',
     operatorsAliases:false,
     dialectOptions:{
